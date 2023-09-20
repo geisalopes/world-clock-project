2d2dac897fed2b00af1774c865ff749a8ca48026
geisalopes <72300375+geisalopes@users.noreply.github.com>
2023-09-20 06:45:36 +0200

    Add the current location

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ setInterval(updtadeTime, 1000);
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let citiesElement = document.querySelector("#cities");
